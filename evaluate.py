14e0a55c5318d6e626540af8cc86fb35043893c2
Bing <bing.rao@outlook.com>
2021-10-08 00:13:04 -0500

    add readme files

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2020 Unknot.id Inc. All Rights Reserved.
+# Copyright 2020 Bingbing Rao. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
